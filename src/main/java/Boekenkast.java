f27dd6ffa1103c4664acb39cd0342f7d611ab9cc
Cédric <cedrictvg@gmail.com>
2023-02-07 21:06:57 +0100

    Added logic to see if it does not fit onto bookshelves

--- src/main/java/Boekenkast.java
+++ src/main/java/Boekenkast.java
@@ -2,12 +2,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
+// TODO: Add logic to sort book title by alphabet
+// TODO: Add logic to get number of required minimum shelves. (https://www.geeksforgeeks.org/sorting-in-java/)
 public class Boekenkast {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		String mainPath = "src/main/resources/";
 		FileReader file = new FileReader(mainPath + "boekenkast/input.txt");
 
+		int bsTotal = 0;
+		int sTotal = 0;
 		try(Scanner scanner = new Scanner(file)) {
 			int t = scanner.nextInt();
 			System.out.println("Number of testcases: " + t + "\n");
@@ -17,7 +21,9 @@ public class Boekenkast {
 				System.out.println("Number of bookshelves: " + bs);
 
 				for (int x = 0; x < bs; x++) {
-					System.out.println("Bookshelve size number #" + (x+1) + " " + scanner.nextInt());
+					int bsS = scanner.nextInt();
+					bsTotal += bsS;
+					System.out.println("Bookshelve size number #" + (x+1) + " " + bsS);
 				}
 
 				int c = scanner.nextInt();
@@ -25,6 +31,7 @@ public class Boekenkast {
 
 				for (int b = 0; b < c; b++) {
 					int s = scanner.nextInt();
+					sTotal += s;
 					System.out.print("Size of book: " + s + " | Title: ");
 
 					StringBuilder title = new StringBuilder();
@@ -34,6 +41,13 @@ public class Boekenkast {
 					System.out.println(title);
 				}
 				System.out.println();
+
+				System.out.println("sTotal: " + sTotal + " | bsTotal: " + bsTotal);
+				if (sTotal > bsTotal) {
+					System.out.println((i + 1) + " ONMOGELIJK");
+				} else {
+					System.out.println((i + 1) + " WIP");
+				}
 			}
 		}
 	}
