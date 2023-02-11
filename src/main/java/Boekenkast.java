ba335ee57f5eb1eb05b8dfb6598d09fe4e17b022
Cédric <cedrictvg@gmail.com>
2023-02-11 15:56:46 +0100

    Solved boekenkast

--- src/main/java/Boekenkast.java
+++ src/main/java/Boekenkast.java
@@ -1,133 +1,57 @@
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
-// TODO: Add logic to get number of required minimum shelves. (https://www.geeksforgeeks.org/sorting-in-java/)
 public class Boekenkast {
 
 	public static void main(String[] args) throws IOException {
-		Calendar calendar = Calendar.getInstance();
-		String formattedDate = (calendar.get(Calendar.MONTH) + 1) + "_" + calendar.get(Calendar.DAY_OF_MONTH) + "_" + calendar.get(Calendar.HOUR_OF_DAY) + "_" + calendar.get(Calendar.MINUTE);
+		try (Scanner scanner = new Scanner(System.in)) {
+			int cases = scanner.nextInt();
+			for (int currentCase = 0; currentCase < cases; currentCase++) {
 
-		// Get's input data
-		String mainPath = "src/main/resources/";
-		FileReader input = new FileReader(mainPath + "boekenkast/input.txt");
-		FileWriter output = new FileWriter(mainPath + "boekenkast/output_" + formattedDate + ".txt");
-
-		/*
-		* bsTotal: total amount of bookshelve space per testcase
-		* sTotal: total amount of book space per testcase
-		* */
-		int bsTotal = 0;
-		int sTotal = 0;
-
-		/*
-		* Tries to read the input data, crashes when reading is unsuccessful
-		* */
-		try(Scanner scanner = new Scanner(input)) {
-
-			/*
-			* Total of testcases in the input file
-			* */
-			int t = scanner.nextInt();
-
-			output.write("Number of testcases: " + t + "\n");
-			output.write("--------------------\n\n");
-
-			/*
-			* Loops through every test case
-			* */
-			for (int i = 0; i < t; i++) {
-				output.write("Test case number " + (i+1) + ":\n");
-				output.write("	Information about the bookshelves:\n");
-
-				/*
-				* bs: total amount of bookshelves in current testcase
-				* bsList: empty list which will be populated with every bookshelve length in the current testcase
-				* */
-				int bs = scanner.nextInt();
-				Integer[] bsList = new Integer[bs];
-
-				output.write("		Number of bookshelves: " + bs + "\n");
-
-				/*
-				* Loops through every bookshelve size
-				* */
-				for (int x = 0; x < bs; x++) {
-					/*
-					* bsS: current bookshelve size
-					* */
-					int bsS = scanner.nextInt();
-
-					bsTotal += bsS;
-
-					/*
-					* Adds current bookshelve length to the list
-					* */
-					bsList[x] = bsS;
-					output.write("		Bookshelve size number " + (x+1) + ": " + bsS + "\n");
+				// Bookshelves
+				int shelves = scanner.nextInt();
+				List<Integer> bookShelves = new ArrayList<>();
+				for (int currentShelve = 0; currentShelve < shelves; currentShelve++) {
+					int currentShelveSize = scanner.nextInt();
+					bookShelves.add(currentShelveSize);
 				}
 
-				output.write("\n		Bookshelve lengths: " + Arrays.toString(bsList).replace("[", "").replace("]", "") +"\n");
+				int books = scanner.nextInt();
+				HashMap<String, Integer> booksUnsorted = new HashMap<>();
+				if (!(books == 0)) {
+					for (int currentBook = 0; currentBook < books; currentBook++) {
+						int currentBookSize = scanner.nextInt();
 
-				/*
-				* Sorts bookshelve lengths in descending order
-				* */
-				Arrays.sort(bsList, Collections.reverseOrder());
-				output.write("		Bookshelve lengths sorted (desc): " + Arrays.toString(bsList).replace("[", "").replace("]", "") + "\n\n");
-				output.write("	Information about the books:\n");
+						StringBuilder title = new StringBuilder();
+						while (!(scanner.hasNextInt()) && scanner.hasNext()) {
+							title.append(scanner.next()).append(" ");
+						}
 
-				/*
-				* Total amount of books in current testcase
-				* */
-				int c = scanner.nextInt();
-				output.write("		Number of books: " + c + "\n");
-
-				/*
-				* Empty list which will be populated with all titles of the current testcase
-				* */
-				String[] titleList = new String[c];
-				for (int b = 0; b < c; b++) {
-					int s = scanner.nextInt();
-					sTotal += s;
-
-					StringBuilder title = new StringBuilder();
-					while(!(scanner.hasNextInt()) && scanner.hasNext()) {
-						title.append(scanner.next()).append(" ").trimToSize();
+						booksUnsorted.put(title.toString().trim(), currentBookSize);
 					}
-
-					output.write("		Size of book: " + s + " | Title: " + title + "\n");
-
-					/*
-					* Adds current title to the list
-					* */
-					titleList[b] = title.toString().trim();
 				}
 
-				output.write("\n		Titles: " + Arrays.toString(titleList).replace("[", "").replace("]", "") + "\n");
+				bookShelves.sort(Collections.reverseOrder());
 
-				/*
-				* Sorts title alphabetically in ascending order
-				* */
-				Arrays.sort(titleList);
+				Map<String, Integer> booksSorted = new TreeMap<>(booksUnsorted);
+				Collection<Integer> bookSizeCollection = booksSorted.values();
+				List<Integer> bookSizes = new ArrayList<>(bookSizeCollection);
 
-				output.write("		Titles sorted (asc): " + Arrays.toString(titleList).replace("[", "").replace("]", "") + "\n\n");
-				output.write("	Total:\n");
-
-				output.write("		sTotal: " + sTotal + " | bsTotal: " + bsTotal + "\n");
-				if (sTotal > bsTotal) {
-					output.write("		Actual answer that needs to be displayed: " + (i+1) + " ONMOGELIJK\n\n");
-				} else {
-					output.write("		Actual answer that needs to be displayed: " + (i+1) + " WIP\n\n");
+				int currentShelf = 0;
+				int count = 0;
+				for (int book : bookSizes) {
+					if (currentShelf < bookShelves.size() && bookShelves.get(currentShelf) >= book) {
+						bookShelves.set(currentShelf, bookShelves.get(currentShelf) - book);
+					} else if (++currentShelf < bookShelves.size() && bookShelves.get(currentShelf) >= book) {
+						bookShelves.set(currentShelf, bookShelves.get(currentShelf) - book);
+						count++;
+					} else {
+						count = -1;
+					}
 				}
-				output.write("--------------------" + ((i == (t - 1)) ? "" : "\n\n"));
+
+				System.out.println((currentCase + 1) + " " + ((count == -1) ? "ONMOGELIJK" : (bookSizes.isEmpty()) ? "0" : (currentShelf + 1)));
 			}
-
-			output.close();
 		}
 	}
 }
