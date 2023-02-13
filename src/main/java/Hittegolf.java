d47a08891dec24e18f55a8febad111d54ee095f4
Cédric <cedrictvg@gmail.com>
2023-02-13 23:16:39 +0100

    Solved hittegolf

--- src/main/java/Hittegolf.java
+++ src/main/java/Hittegolf.java
@@ -1,63 +1,58 @@
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
 
 public class Hittegolf {
 
 	public static void main(String[] args) throws IOException {
-		Calendar calendar = Calendar.getInstance();
-		String date = calendar.get(Calendar.DAY_OF_MONTH) + "_" + calendar.get(Calendar.MONTH) + "_" + calendar.get(Calendar.HOUR_OF_DAY) + "_" + calendar.get(Calendar.MINUTE) + "_" + calendar.get(Calendar.SECOND);
-
-		String path = "src/main/resources/";
-		FileReader input = new FileReader(path + "hittegolf/input.txt");
-		FileWriter output = new FileWriter(path + "hittegolf/output_" + date + ".txt");
-
-		try (Scanner scanner = new Scanner(input)) {
+		try (Scanner scanner = new Scanner(System.in)) {
 			int cases = scanner.nextInt();
-			output.write("Total testcases: " + cases);
 
-			// testcases
 			for (int currentCase = 0; currentCase < cases; currentCase++) {
-				output.write("\n\nCurrent testcase: " + (currentCase+1));
 
-				// temperatures
-				int tempCount = 1;
 				List<Double> temperatures = new ArrayList<>();
 				while (scanner.hasNextDouble()) {
 					double currentTemperature = scanner.nextDouble();
 					temperatures.add(currentTemperature);
-					output.write("\n	Temperature reading #" + tempCount + ": " + currentTemperature);
-					tempCount++;
 				}
 
-				/*
-				Checks:
-					- temperature => 25
-					- temperature < 25 -> tempList = empty or list contains 5 temps (>=3 -> 30+)
-				End checks:
-					- Total # temps >= 5 (>=3 -> 30+)
-				 */
-				// heatwave
+				double prev = -1;
+				int thirty = 0;
+				int count = 0;
+				int heatWaveStart = -1;
 				List<Double> heatWave = new ArrayList<>();
 				for (double temp : temperatures) {
+					if (temp < 25) {
+						if (heatWave.size() >= 5 && thirty >= 3) {
+							break;
+						}
+						heatWave.clear();
+						thirty = 0;
+						count++;
+						heatWaveStart = -1;
+						prev = temp;
+						continue;
+					}
 
+					if (temp >= 30) {
+						thirty++;
+					}
+
+					if (prev >= 25 || prev == -1 || heatWave.isEmpty()) {
+						if (heatWave.isEmpty()) {
+							heatWaveStart = count+1;
+						}
+						heatWave.add(temp);
+						count++;
+					}
+
+					prev = temp;
 				}
 
-//				System.out.println((currentCase+1) + " " + (heatWaveStart == -1 ? "geen hittegolf" : (heatWaveStart+1) + " " + heatWave.size()));
-
-				output.write("\n\nTemperature list: " + temperatures);
-				output.write("\nPredicted heatwave: " + heatWave);
-//				output.write("\nStart of heatwave: " + ((heatWaveStart == -1) ? "no heatwave" : (heatWaveStart+1)));
-
-				output.write("\n-----");
+				System.out.println((currentCase+1) + " " + (heatWaveStart == -1 ? "geen hittegolf" : (heatWaveStart) + " " + heatWave.size()));
 				scanner.next();
 			}
-
-			output.close();
 		}
 	}
 }
